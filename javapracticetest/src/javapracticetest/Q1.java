51e27af0697e729b612c16d35a26ecc7d5ac4042
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2023-01-06 12:57:14 +0530

    Add files via upload

---
+++ javapracticetest/src/javapracticetest/Q1.java
@@ -0,0 +1,10 @@
+package javapracticetest;
+import java.util.*;
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scnner sc = new Scanner(System.in)
+	}
+
+}
